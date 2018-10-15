2e5946b38fa4e25f4a66a76c46c1704172db85f4
sheyla maribi cu yah <15470143itcampeche.edu.mx>
2018-10-14 21:35:36 -0500

    ContadorApp: Se agregó el manejo de eventos a la interfaz de usuario gráfica.

--- app/src/main/java/com/sheylacu/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/sheylacu/android/contadorapp/MainActivity.java
@@ -1,10 +1,17 @@
 package com.sheylacu.android.contadorapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button BotonContar, BotonReiniciar;
+    private EditText campoConteo;
+    private int Contador =0;
+    private  int Reinicio = 0;
 
 
     @Override
@@ -12,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        campoConteo = (EditText) findViewById(R.id.campoConteo);
+        BotonContar = (Button) findViewById(R.id.botonContar);
+        BotonReiniciar = (Button) findViewById(R.id.botonReiniciar);
+
+        BotonContar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                Contador++;
+
+                campoConteo.setText("Contador: " +Contador);
+            }
+        });
+
+        BotonReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reinicio = 0;
+                campoConteo.setText("0");
+
+
+            }
+        });
 
     }
 
